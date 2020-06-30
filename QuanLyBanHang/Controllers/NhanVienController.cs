b753fadb1412111cbe27f723e901ff45bc2af6ef
Trần Anh Tuân <tuantrananhit@gmail.com>
2020-06-30 14:10:15 +0700

    delete 2 line duplicate

--- QuanLyBanHang/Controllers/NhanVienController.cs
+++ QuanLyBanHang/Controllers/NhanVienController.cs
@@ -82,40 +82,6 @@ namespace QuanLyBanHang.Controllers
             return new FileStreamResult(workStream, "application/pdf");
         }
 
-        public byte[] GetPDF(string pHTML)
-        {
-            byte[] bPDF = null;
-
-            MemoryStream ms = new MemoryStream();
-            TextReader txtReader = new StringReader(pHTML);
-
-            // 1: create object of a itextsharp document class
-            iTextSharp.text.Document doc = new iTextSharp.text.Document(PageSize.A4, 25, 25, 25, 25);
-
-            // 2: we create a itextsharp pdfwriter that listens to the document and directs a XML-stream to a file
-            PdfWriter oPdfWriter = PdfWriter.GetInstance(doc, ms);
-
-            // 3: we create a worker parse the document
-            HTMLWorker htmlWorker = new HTMLWorker(doc);
-
-            // 4: we open document and start the worker on the document
-            doc.Open();
-            htmlWorker.StartDocument();
-
-            // 5: parse the html into the document
-            htmlWorker.Parse(txtReader);
-
-            // 6: close the document and the worker
-            htmlWorker.EndDocument();
-            htmlWorker.Close();
-            doc.Close();
-
-            bPDF = ms.ToArray();
-
-            return bPDF;
-        }
-
-
 
         // GET: NhanVien/Details/5
         public async Task<IActionResult> Details(int? id)
