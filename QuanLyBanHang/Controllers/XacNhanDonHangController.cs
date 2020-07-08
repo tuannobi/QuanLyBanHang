a5375b3cfc8ca3cc319d90e91c287624bdc40ebf
nhuyle287 <50352152+nhuyle287@users.noreply.github.com>
2020-07-08 21:40:26 +0700

    update in ds hoadon

--- QuanLyBanHang/Controllers/XacNhanDonHangController.cs
+++ QuanLyBanHang/Controllers/XacNhanDonHangController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
-using GrapeCity.Documents.Html;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
--- QuanLyBanHang/Startup.cs
+++ QuanLyBanHang/Startup.cs
@@ -75,7 +75,7 @@ namespace QuanLyBanHang
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=HomePage}/{action=Index}/{id?}");
+                    pattern: "{controller=HoaDon}/{action=Details}/{id?}");
             });
         }
     }
