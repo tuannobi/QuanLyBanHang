45f9af11d6d7bb4b9febea5b7a4eeb6d17169bfe
Trần Anh Tuân <tuantrananhit@gmail.com>
2020-07-09 10:18:25 +0700

    xem file HoaDon/Details va HoaDon/Index1.cshtml

--- QuanLyBanHang/Startup.cs
+++ QuanLyBanHang/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using QuanLyBanHang.Models;
+using Rotativa.AspNetCore;
 
 namespace QuanLyBanHang
 {
